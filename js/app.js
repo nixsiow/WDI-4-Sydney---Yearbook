641ada8d09531283dc019ec827b144fe0b999ac0
=-kriss-= <kriss@modelcitizen.com.au>
2014-04-26 16:38:50 +1000

    updated views

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 var app = app || {};
-var ENTER_KEY = 13;
 
 $(document).ready(function() {
   new app.AppView();
--- js/views/app.js
+++ js/views/app.js
@@ -1,89 +1,23 @@
 var app = app || {};
 
 app.AppView = Backbone.View.extend({
-  el: '#todoapp',
-  statsTemplate: _.template($('#stats-template').html()),
+  el: '#yearbookapp',
   events: {
-    'keypress #new-todo': 'createOnEnter',
-    'click #clear-completed': 'clearCompleted',
-    'click #toggle-all': 'toggleAllComplete'
+    this.listenTo(app.Students, 'student', this.showStudent)
+    // add a click event for each student in the view
   },
   initialize: function () {
-    this.allCheckbox = this.$('#toggle-all').get(0);
-    this.$input = this.$('#new-todo');
-    this.$footer = this.$('#footer');
-    this.$main = this.$('#main');
-
-    this.listenTo(app.Todos, 'add', this.addOne);
-    this.listenTo(app.Todos, 'reset', this.addAll);
-
-    this.listenTo(app.Todos, 'change:completed', this.filterOne);
-    this.listenTo(app.Todos, 'filter', this.filterAll);
-    this.listenTo(app.Todos, 'all', this.render);
-
-    app.Todos.fetch();
+    this.template = _.template($('#appView').html());
   },
   render: function () {
-    var completed = app.Todos.completed().length;
-    var remaining = app.Todos.remaining().length;
+    // get this template and stick it into the el: element on page
+    this.$el.html(this.template());
 
-    if (app.Todos.length) {
-      this.$main.show();
-      this.$footer.show();
-
-      this.$footer.html(this.statsTemplate({
-        completed: completed,
-        remaining: remaining
-      }));
-
-      this.$('#filters li a')
-        .removeClass('selected')
-        .filter('[href="#/' + (app.TodoFilter || '') + '"]')
-        .addClass('selected');
-    } else {
-      this.$main.hide();
-      this.$footer.hide();
-    }
-    this.allCheckbox.checked = !remaining;
-  },
-  addOne: function (todo) {
-    var view = new app.TodoView({model: todo});
-    $('#todo-list').append(view.render().el);
-  },
-  addAll: function () {
-    this.$('#todo-list').empty();
-    app.Todos.each(this.addOne, this);
-  },
-  filterOne: function (todo) {
-    todo.trigger('visible');
-  },
-  filterAll: function () {
-    app.Todos.each(this.filterOne, this);
-  },
-  newAttributes: function () {
-    return {
-      title: this.$input.val().trim(),
-      order: app.Todos.nextOrder(),
-      completed: false
-    };
-  },
-  createOnEnter: function () {
-    if (event.which != ENTER_KEY || !this.$input.val().trim()) {
-      return;
-    }
-    app.Todos.create(this.newAttributes());
-    this.$input.val('');
-  },
-  clearCompleted: function () {
-    _.invoke(app.Todos.completed(), 'destroy');
-    return false;
-  },
-  toggleAllComplete: function () {
-    var completed = this.allCheckbox.checked;
-    app.Todos.each(function (todo) {
-      todo.save({
-        'completed': completed
-      })
-    })
+    // now send all of this to the view for each item int he collections
+    this.collection.each(function(student){
+      var view = new StudentListView({model: student});
+      view.render();
+      $('#student').append(view.el);
+    });
   }
 });
